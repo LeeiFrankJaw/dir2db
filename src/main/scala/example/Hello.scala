37782b481f404a94b5d2493a9c2b8c5e1b673e09
Lei Zhao <LeiZh26@gmail.com>
2019-02-26 22:08:56 +0800

    use MySQLInnoDBAdapter to save relation
    MySQLAdapter was for the old MyISAM engine.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,32 +1,56 @@
 package example
 
-import org.squeryl.{
-  Session,
-  SessionFactory,
-  Schema,
-  KeyedEntity,
-  ForeignKeyDeclaration
-}
-import org.squeryl.dsl.{OneToMany,ManyToOne}
-import org.squeryl.adapters.MySQLAdapter
-import org.squeryl.PrimitiveTypeMode._
-import org.squeryl.annotations.Column
-import java.util.Date
-import java.sql.Timestamp
+import java.nio.file.Paths
 import java.sql.{Connection,DriverManager}
+import java.sql.Timestamp
+import java.time.Instant
+import java.util.Date
+import org.squeryl.{
+  ForeignKeyDeclaration,
+  KeyedEntity,
+  KeyedEntityDef,
+  Schema,
+  Session,
+  SessionFactory
+}
+import org.squeryl.PrimitiveTypeMode._
+import org.squeryl.adapters.MySQLInnoDBAdapter
+import org.squeryl.annotations.Column
+import org.squeryl.dsl.{OneToMany,ManyToOne}
+import os.{Path,RelPath}
+
+object FileType extends Enumeration {
+  type FileType = Value
+  val File = Value(0, "File")
+  val Directory = Value(1, "Directory")
+}
 
 
-class File (
-  val id: Long,
+class File(
   val path: String,
-  val fielname: String,
-  val fileType: String,
+  val filename: String,
+  val fileType: FileType.FileType,
   val parent: Long,
   val size: Long,
   val mtime: Timestamp,
   val atime: Timestamp,
   val ctime: Timestamp,
-)
+) extends KeyedEntity[Long] {
+  def this() = this("", "",
+    FileType.File, 0, 0,
+    new Timestamp(0),
+    new Timestamp(0),
+    new Timestamp(0))
+  var id = 0
+  lazy val files: OneToMany[File] = FileDb.filesToFiles.left(this)
+  lazy val parentDir: ManyToOne[File] = FileDb.filesToFiles.right(this)
+}
+
+object FileDb extends Schema {
+  val files = table[File]
+  val filesToFiles = oneToManyRelation(files, files)
+    .via((o, m) => o.id === m.parent)
+}
 
 class Author(
   val firstName: String,
@@ -41,13 +65,13 @@ class Author(
 
 
 class Book(
-  var id: Long,
   var title: String,
   @Column("AUTHOR_ID")
   var authorId: Long,
   var coAuthorId: Option[Long]
-) {
-  def this() = this(0, "", 0, Some(0L))
+) extends KeyedEntity[Long] {
+  def this() = this("", 0, Some(0L))
+  var id: Long = 0
   lazy val author: ManyToOne[Author] = Library.authorsToBooks.right(this)
 }
 
@@ -71,30 +95,52 @@ object Hello extends Greeting with App {
   SessionFactory.concreteFactory = Some(() =>
     Session.create(
       java.sql.DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/dir2db?user=root"),
-      new MySQLAdapter))
+        "jdbc:mysql://localhost:3306/dir2db?user=root&characterEncoding=UTF-8"),
+      new MySQLInnoDBAdapter))
 
   transaction {
     Session.currentSession.setLogger(println)
-    Library.create
-    Library.printDdl
-    authors.insert(new Author("Michel", "Folco", Some("folco@example.com")))
-    val a = from(authors)(a => where(a.lastName === "Folco") select a)
-    books.insert(new Book(0, "Can't Use Squeryl", 15, None))
+    // println(Session.currentSession.databaseAdapter.supportsForeignKeyConstraints)
+    // Library.create
+    // Library.printDdl
+    // authors.insert(new Author("Michel", "Folco", Some("folco@example.com")))
+    // val a = from(authors)(a => where(a.lastName === "Folco") select a)
+    // books.insert(new Book(0, "Can't Use Squeryl", 15, None))
     // println(a)
+    // FileDb.printDdl
+    // FileDb.create
+    // val s = Session.currentSession.connection.createStatement
+    // s.execute("""ALTER TABLE Book ADD
+    //             |CONSTRAINT BookFK1
+    //             |FOREIGN KEY (AUTHOR_ID) REFERENCES AUTHORS(id)""".stripMargin)
+    // s.execute("""ALTER TABLE FILE ADD
+    //             |CONSTRAINT FileFK1
+    //             |FOREIGN KEY (parent) REFERENCES File(id)""".stripMargin)
+    // s.execute("""ALTER TABLE File
+    //             |CHARSET utf8mb4""".stripMargin)
+    walk(".").foreach(println(_))
   }
 
-  println(greeting)
-  val wd = os.pwd
-  for (path <- os.walk.stream(
-    wd,
-    p => (Seq("project", "target") contains p.last) || p.last.startsWith("."))
-  ) {
-    val info = os.stat.full(path)
-    val mtime = Timestamp.from(info.mtime.toInstant)
-    // println(s"${mtime}\t${info.atime}\t${info.ctime}")
-    println(s"${mtime}\t${info.mtime}\t${path.last}")
-  }
+  // def autoPath(str: String): Path = {
+  //   val p = Paths.get(str)
+  //   if (p.isAbsolute) Path(p) else RelPath(p)
+  // }
+
+
+  def walk(str: String) =
+    for (path <- os.walk.stream(
+      Path.expandUser(str, os.pwd),
+      p => (Seq("project", "target") contains p.last) || p.last.startsWith("."),
+      followLinks = true,
+      includeTarget = true)
+    ) yield {
+      val info = os.stat.full(path)
+      // os.perms
+      // os.owner
+      val mtime = Timestamp.from(info.mtime.toInstant)
+      // println(s"${mtime}\t${info.atime}\t${info.ctime}")
+      s"${mtime}\t${info.mtime}\t${path.last}"
+    }
 }
 
 trait Greeting {
